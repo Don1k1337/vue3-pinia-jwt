07cef180a8008758580dc97173ac321275de1b04
Don1k1337 <dlyaspamnyhpisem@gmail.com>
2023-07-17 11:25:20 +0600

    feat: vite.config change

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import SignInView from '@/views/SignInView.vue'
 import DashboardView from '@/views/DashboardView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_APP_URL),
   routes: [
     {
       path: '/',
--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: import.meta.env.VITE_APP_URL
 })
